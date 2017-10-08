4918c1a683cb22c880292bb61fffeccd9aa9f2e1
Tiernan Martin <tiernan.martin@gmail.com>
2017-10-08 02:05:57 +0000

    added a remove_files param to mk_shiny_pkg_dir()

--- R/mk_shiny_pkg_dir.R
+++ R/mk_shiny_pkg_dir.R
@@ -2,8 +2,9 @@
 #'
 #' \code{mk_shiny_pkg_dir} creates a directory of folders for a Shiny Application package.
 #'
-#' These folders will also contain \code{/ui.R}, \code{/server.R}, and \code{/global.R}.
-#' @note This file director comes from the a \href{https://community.rstudio.com/t/best-practices-shiny-development/1694/5?u=tiernan}{post} in the RStudio Community Forum
+#' @param remove_files logical, whether to delete all files in \code{man/} and \code{R/} (defaults to \code{FALSE})
+#' @note These folders will also contain \code{/ui.R}, \code{/server.R}, and \code{/global.R}.
+#' @note This file directory comes from a \href{https://community.rstudio.com/t/best-practices-shiny-development/1694/5?u=tiernan}{post} in the \href{https://community.rstudio.com/}{RStudio Community Forum}.
 #' @importFrom readr "read_rds"
 #' @importFrom purrr "pluck"
 #' @importFrom purrr "map_chr"
@@ -12,7 +13,7 @@
 #' @importFrom magrittr "%>%"
 #' @importFrom stringr "str_c"
 #' @export
-mk_shiny_pkg_dir <- function(){
+mk_shiny_pkg_dir <- function(remove_files = FALSE){
 
   # Read the directory template file from /data
 
@@ -41,6 +42,18 @@ mk_shiny_pkg_dir <- function(){
     map_chr(~ str_c('touch ', .x, collapse = "")) %>%
     walk(system)
 
+  # Remove the junk files in ./man/ and /.R/
+  if(remove_files){
+    c('man', 'R') %>%
+      map_chr(~ str_c('rm ',.x,"/*",collapse = "")) %>%
+      walk(system)
+  }
+
+
+   # Add `.gitkeep` files to each folder
+   c('man', 'R') %>%
+     map_chr(~ str_c('touch ',.x,"/.gitkeep",collapse = "")) %>%
+     walk(system)
 
   # system("touch .nojekyll")  # this is no longer necessary
   return(NULL)
